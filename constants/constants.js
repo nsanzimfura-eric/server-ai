c773d2dca814fe1a51633ac1603c7695faf89432
Nsanzimfura Eric <erickykress1@gmail.com>
2023-10-04 02:14:05 +0200

    Update constants.js

--- constants/constants.js
+++ constants/constants.js
@@ -1,4 +1,4 @@
-const hostname = "https://realassist-ai-challenge.vercel.app/";
+const hostname = "https://realassist-ai-qcu0.onrender.com/";
 // const hostname = "http://localhost:3000/";
 
 module.exports = { hostname };
