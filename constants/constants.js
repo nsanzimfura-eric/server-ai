2903d30e5d32a78c038c83bc77006c39509c860f
Nsanzimfura Eric <erickykress1@gmail.com>
2023-10-04 18:42:21 +0200

    Update constants.js

--- constants/constants.js
+++ constants/constants.js
@@ -1,4 +1,4 @@
-const hostname = "https://realassist-ai-qcu0.onrender.com/";
+const hostname = "https://realassist-ai-qcu0.onrender.com/download";
 // const hostname = "http://localhost:3000/";
 
 module.exports = { hostname };
